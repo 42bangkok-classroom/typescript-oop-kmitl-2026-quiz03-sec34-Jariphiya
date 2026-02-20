a43abde870e2b4afc17fcbcbb059fafce713da19
Jariphiya <68010130@kmitl.ac.th>
2026-02-20 18:34:24 +0700

    jaijdadid

--- src/practicals/p04/online-course.ts
+++ src/practicals/p04/online-course.ts
@@ -1,1 +1,40 @@
-export class OnlineCourse {}
+export class OnlineCourse {
+  courseName: string;
+  maxStudents: number;
+  private enrolledStudents: number;
+  private isOpen: boolean;
+
+  constructor(courseName: string, maxStudents: number) {
+    this.courseName = courseName;
+    this.maxStudents = maxStudents;
+    this.enrolledStudents = 0;
+    this.isOpen = true;
+  }
+
+  private canEnroll(): boolean {
+    return this.isOpen && this.enrolledStudents < this.maxStudents;
+  }
+
+  enroll(): boolean {
+    if (this.canEnroll()) {
+      this.enrolledStudents++;
+      return true;
+    }
+    return false;
+  }
+
+  closeCourse(): void {
+    this.isOpen = false;
+  }
+
+  getAvailableSeats(): number {
+    return this.maxStudents - this.enrolledStudents;
+  }
+
+  getCourseStatus(): string {
+    if (this.isOpen) {
+      return "Open";
+    }
+    return "Closed";
+  }
+}
