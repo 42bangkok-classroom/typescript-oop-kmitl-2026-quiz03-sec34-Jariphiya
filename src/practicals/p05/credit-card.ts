ab24f788dd083e71d805f515491f1d56b718d594
Jariphiya <68010130@kmitl.ac.th>
2026-02-20 18:59:32 +0700

    smfsf

--- src/practicals/p05/credit-card.ts
+++ src/practicals/p05/credit-card.ts
@@ -1,1 +1,20 @@
-export class CreditCard {}
+import {PaymentMethod} from "./payment-method"
+import {Payable} from "./payable"
+
+export class CreditCard extends PaymentMethod {
+    private _cardNumber:string = ""
+    
+    constructor(accountName:string,cardNumber:string){
+        super(accountName)
+        this._cardNumber = cardNumber
+    }
+
+
+    processPayment(amount: number): boolean {
+        return true
+    }
+
+    getPaymentFee(amount: number): number {
+        return amount * 0.02
+    }
+}
--- src/practicals/p05/payment-method.ts
+++ src/practicals/p05/payment-method.ts
@@ -1,7 +1,16 @@
-export class PaymentMethod {
+import {Payable} from "./payable"
+
+export abstract class PaymentMethod implements Payable {
     protected accountName:string = ""
 
     constructor(accountName:string){
         this.accountName = accountName
     }
+
+    getAccountName(){
+        return this.accountName
+    }
+
+    abstract processPayment(amount: number): boolean;
+    abstract getPaymentFee(amount: number): number;
 }
