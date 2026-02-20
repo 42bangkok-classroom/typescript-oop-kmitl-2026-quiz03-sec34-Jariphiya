0667137ef1b3f2acb397d471227914b6b03e2971
Jariphiya <68010130@kmitl.ac.th>
2026-02-20 19:06:16 +0700

    sfdfd

--- src/practicals/p05/ewallet.ts
+++ src/practicals/p05/ewallet.ts
@@ -1,1 +1,19 @@
-export class EWallet {}
+import {PaymentMethod} from "./payment-method"
+import {Payable} from "./payable"
+
+export class EWallet extends PaymentMethod{
+    private _walletId:string
+
+    constructor(accountName:string,walletId:string){
+        super(accountName)
+        this._walletId = walletId
+    }
+
+    processPayment(amount: number): boolean {
+        return true
+    }
+
+    getPaymentFee(amount: number): number {
+        return amount * 0.01
+    }
+}
