15e9adb460cec4e8b27c879dc15496dfc384dd41
Jariphiya <68010130@kmitl.ac.th>
2026-02-20 17:38:21 +0700

    prac2

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,26 @@
-export class User {}
+export class User {
+    firstname:string = ""
+    lastname:string = ""
+    private _age:number = 0
+    static BIRTH_YEAR = 2000
+
+    setFirstname(firstname:string){
+        this.firstname = firstname
+    }
+
+    setLastname(lastname:string){
+        this.lastname = lastname
+    }
+
+    setAge(ages:number){
+        this._age = ages
+    }
+
+    getAge(){
+        return this._age
+    }
+
+    getFullname(){
+        return `${this.firstname} ${this.lastname}`
+    }
+}
